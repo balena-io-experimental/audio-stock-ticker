7a9cc679654da1bc20ad370b387f78752949dd37
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-07-07 22:53:01 +0100

    add stock scraper

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var request = require('request');
 var Lame = require('lame');
 var Speaker = require('speaker');
+var stockscraper = require('stockscraper');
 
-var text = 'Say hello to my little friend';
+stockscraper.scrape('NASDAQ', 'AMZN', function(err, data) {
+  text = 'The current price of Amazon stock is ' + data['l'] + ' dollars'
+  console.log('The current price of Amazon stock is ' + data['l'] +
+    ' dollars');
 
-var url = 'http://translate.google.com/translate_tts?tl=en&q=' +
-  encodeURIComponent(text);
-request(url).pipe(new Lame.Decoder).pipe(new Speaker);
+  var url = 'http://translate.google.com/translate_tts?tl=en&q=' +
+    encodeURIComponent(text);
+  request(url).pipe(new Lame.Decoder).pipe(new Speaker);
+});
